99e3b72067214510ee8b036c5d6baca8e7e72f06
Chris Davis <cdav49719@.com>
2020-07-18 22:03:22 -0700

    Updates

---
+++ precache-manifest.8f3cb4a0558b2dcaa29ddc74877bd632.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d278659e2b6167b75316072e1cd41022",
+    "url": "/g7web/index.html"
+  },
+  {
+    "revision": "24dcbf4ee64b294b952d",
+    "url": "/g7web/static/css/main.c440bba9.chunk.css"
+  },
+  {
+    "revision": "3e92d325e453788375cd",
+    "url": "/g7web/static/js/2.685fa3c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/g7web/static/js/2.685fa3c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24dcbf4ee64b294b952d",
+    "url": "/g7web/static/js/main.f6bf63ff.chunk.js"
+  },
+  {
+    "revision": "66800fb37f969b863cfe",
+    "url": "/g7web/static/js/runtime-main.0a9c57e6.js"
+  },
+  {
+    "revision": "b0aed5a40fdec7b1bb31c86d4ec8ed7b",
+    "url": "/g7web/static/media/Video1.b0aed5a4.jpg"
+  },
+  {
+    "revision": "d0616b2ebaba77b2841d601fa1d2b7b4",
+    "url": "/g7web/static/media/photosPic.d0616b2e.jpeg"
+  }
+]);
